9f23b6ad477dfa3fe3e3e4167165541e0d57840b
Mauro Crispim <mcrispim@hotmail.com>
2023-05-03 18:10:27 -0300

    Adequa a classe DisneyCharactersResult à API da Disney

--- app/src/main/java/com/example/listapersonagens/network/service/disney/DisneyCharactersResult.kt
+++ app/src/main/java/com/example/listapersonagens/network/service/disney/DisneyCharactersResult.kt
@@ -1,10 +1,31 @@
 package com.example.listapersonagens.network.service.disney
 
 data class DisneyCharactersResult(
-    val data: List<DisneyCharacter>
+    val data: List<Data>,
+    val info: Info
 )
 
-data class DisneyCharacter(
+data class Data(
+    val __v: Int,
+    val _id: Int,
+    val allies: List<Any>,
+    val createdAt: String,
+    val enemies: List<Any>,
+    val films: List<String>,
+    val imageUrl: String,
     val name: String,
-    val imageUrl: String
+    val parkAttractions: List<String>,
+    val shortFilms: List<String>,
+    val sourceUrl: String,
+    val tvShows: List<String>,
+    val updatedAt: String,
+    val url: String,
+    val videoGames: List<String>
+)
+
+data class Info(
+    val count: Int,
+    val nextPage: String,
+    val previousPage: Any,
+    val totalPages: Int
 )
