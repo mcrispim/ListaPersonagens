2cd852caab980aa1a3d1fa05645a8b0dc2f12cba
Mauro Crispim <mcrispim@hotmail.com>
2023-05-04 17:48:53 -0300

    Altera CharactersFragment para utilizar o viewmodel

--- app/src/main/java/com/example/listapersonagens/ui/fragment/CharactersFragment.kt
+++ app/src/main/java/com/example/listapersonagens/ui/fragment/CharactersFragment.kt
@@ -6,22 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.listapersonagens.R
-import com.example.listapersonagens.databinding.FragmentCharactersBinding
 import com.example.listapersonagens.data.model.CharacterType.DISNEY
 import com.example.listapersonagens.data.model.CharacterType.RICKY_AND_MORTY
-import com.example.listapersonagens.model.mapper.toDomain
-import com.example.listapersonagens.data.network.service.disney.DisneyService
-import com.example.listapersonagens.data.network.service.rm.RickyAndMortyService
+import com.example.listapersonagens.databinding.FragmentCharactersBinding
 import com.example.listapersonagens.ui.utils.adapter.CharactersAdapter
 import com.example.listapersonagens.ui.utils.extension.gone
 import com.example.listapersonagens.ui.utils.extension.visible
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import com.example.listapersonagens.viewmodels.CharactersFragmentViewModel
 
 /*
 - Qual o princípio e suas características?
@@ -107,88 +101,66 @@ import retrofit2.converter.gson.GsonConverterFactory
 */
 
 class CharactersFragment : Fragment() {
-    
+
     private var _binding: FragmentCharactersBinding? = null
     private val binding get() = _binding!!
-    
-    private val retrofitDisney: Retrofit = Retrofit.Builder()
-        .baseUrl("https://api.disneyapi.dev/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-    private val disneyService: DisneyService = retrofitDisney.create(DisneyService::class.java)
-    
-    private val retrofitRickyAndMorty: Retrofit = Retrofit.Builder()
-        .baseUrl("https://rickandmortyapi.com/api/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-    private val rickyAndMortyService: RickyAndMortyService =
-        retrofitRickyAndMorty.create(RickyAndMortyService::class.java)
-    
+
+    private lateinit var viewModel: CharactersFragmentViewModel
+    //private var viewModel: CharactersFragmentViewModel by viewModels()
+
     private val charactersAdapter = CharactersAdapter()
-    
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-    
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         _binding = FragmentCharactersBinding.inflate(inflater, container, false)
         return binding.root
     }
-    
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupView()
+        //val viewModel: CharactersFragmentViewModel by viewModels()
+        setupView(viewModel)
     }
-    
-    private fun setupView() {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CharactersFragmentViewModel::class.java)
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupView(viewModel: CharactersFragmentViewModel) {
         with(binding) {
             rvCharacters.adapter = charactersAdapter
-            
+
             rgCharacterType.setOnCheckedChangeListener { _, checkedId ->
                 when (checkedId) {
-                    R.id.rbCharacterTypeDisney -> {
-                        CoroutineScope(Dispatchers.IO).launch(Dispatchers.Main) {
-                            pbLoadingCharacters.visible()
-                            tvCharactersTypeTitle.text = DISNEY.title
-                            llCharactersTypeHeader.background = AppCompatResources.getDrawable(
-                                requireContext(),
-                                DISNEY.colorRes
-                            )
-                            Glide.with(binding.root)
-                                .load(DISNEY.imageUrl)
-                                .error(R.drawable.ic_launcher_background)
-                                .into(ivCharactersTypeImage)
-                            
-                            val disneyCharacters = disneyService.getCharacters()
-                            charactersAdapter.submitList(disneyCharacters.data.toDomain())
-                            pbLoadingCharacters.gone()
-                        }
-                    }
-                    R.id.rbCharacterTypeRickyAndMorty -> {
-                        CoroutineScope(Dispatchers.IO).launch(Dispatchers.Main) {
-                            pbLoadingCharacters.visible()
-                            tvCharactersTypeTitle.text = RICKY_AND_MORTY.title
-                            llCharactersTypeHeader.background = AppCompatResources.getDrawable(
-                                requireContext(),
-                                RICKY_AND_MORTY.colorRes
-                            )
-                            Glide.with(binding.root)
-                                .load(RICKY_AND_MORTY.imageUrl)
-                                .error(R.drawable.ic_launcher_background)
-                                .into(ivCharactersTypeImage)
-                            
-                            val rickyAndMortyCharacters = rickyAndMortyService.getCharacters()
-                            charactersAdapter.submitList(
-                                rickyAndMortyCharacters.results.toDomain()
-                            )
-                            pbLoadingCharacters.gone()
-                        }
-                    }
+                    R.id.rbCharacterTypeDisney -> viewModel.setCharacterType(DISNEY)
+                    R.id.rbCharacterTypeRickyAndMorty -> viewModel.setCharacterType(RICKY_AND_MORTY)
                 }
+            }
+
+            viewModel.characterType.observe(viewLifecycleOwner) {
+                pbLoadingCharacters.visible()
+                tvCharactersTypeTitle.text = it.title
+                llCharactersTypeHeader.background = AppCompatResources.getDrawable(
+                    requireContext(),
+                    it.colorRes
+                )
+                Glide.with(binding.root)
+                    .load(it.imageUrl)
+                    .error(R.drawable.ic_launcher_background)
+                    .into(ivCharactersTypeImage)
+            }
+
+            viewModel.characters.observe(viewLifecycleOwner) {
+                charactersAdapter.submitList(it)
+                pbLoadingCharacters.gone()
             }
         }
     }
