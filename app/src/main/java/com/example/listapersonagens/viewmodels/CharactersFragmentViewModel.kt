aedb4b56438446e35a5e9d633bdc8644de9a8674
Mauro Crispim <mcrispim@hotmail.com>
2023-05-06 14:36:37 -0300

    Atualiza a lista de caracteres na thread principal

--- app/src/main/java/com/example/listapersonagens/viewmodels/CharactersFragmentViewModel.kt
+++ app/src/main/java/com/example/listapersonagens/viewmodels/CharactersFragmentViewModel.kt
@@ -1,20 +1,17 @@
 package com.example.listapersonagens.viewmodels
 
-import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.listapersonagens.data.model.Charackter
 import com.example.listapersonagens.data.model.CharacterType
 import com.example.listapersonagens.data.repository.CharactersRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class CharactersFragmentViewModel() : ViewModel() {
+class CharactersFragmentViewModel : ViewModel() {
 
     private val _characters = MutableLiveData<List<Charackter>>()
     val characters: LiveData<List<Charackter>>
@@ -24,11 +21,15 @@ class CharactersFragmentViewModel() : ViewModel() {
     val characterType: LiveData<CharacterType>
         get() = _characterType
 
-    val repository = CharactersRepository()
+    private val repository = CharactersRepository()
+
     fun setCharacterType(characterType: CharacterType) {
         _characterType.value = characterType
         viewModelScope.launch(Dispatchers.IO) {
-            _characters.value = repository.getCharacters(characterType)
+            val charactersResult = repository.getCharacters(characterType)
+            withContext(Dispatchers.Main) {
+                _characters.value = charactersResult
+            }
         }
     }
 
