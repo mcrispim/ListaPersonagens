6d6801ecafb772557cb17cfc5693f262b1dfbc75
Mauro Crispim <mcrispim@hotmail.com>
2023-05-07 12:31:45 -0300

    Cria a classe ListaCaracteresApplication

---
+++ app/src/main/java/com/example/listapersonagens/ListaCaracteresApplication.kt
@@ -0,0 +1,8 @@
+package com.example.listapersonagens
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ListaCaracteresApplication: Application() {
+}
